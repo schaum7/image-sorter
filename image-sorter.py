0daddbd1666d8d0a7569681e8fe8295b34448727
Schaum7 <falk.altrock@gmail.com>
2020-02-23 14:53:10 +0100

    list all dates from jpg files in a folder works

--- image-sorter.py
+++ image-sorter.py
@@ -1,13 +1,26 @@
 # initial
 from exif import Image
+import os
 
 
 def get_date(image: Image):
     if image.has_exif:
-        return str(my_image.datetime).split(" ")[0].split(":")
+        return str(image.datetime).split(" ")[0].split(":")
 
 
-with open('example-images\\Camera\\20190803_083141.jpg', 'rb') as image_file:
-    my_image = Image(image_file)
+def get_images(path: str):
+    images = []
+    for image in os.listdir(path):
+        if image.endswith(".jpg"):
+            images.append(os.path.join(path, image))
+    return images
 
-print(get_date(my_image))
+
+images = get_images("example-images\\Camera")
+for image in images:
+    with open(image, 'rb') as image_file:
+        my_image = Image(image_file)
+    print(get_date(my_image))
+
+
+# print(get_date(my_image))
