3f8141db373af58874ca7020fe294166606eca2e
Schaum7 <falk.altrock@gmail.com>
2020-02-23 13:51:16 +0100

    reading date works

--- image-sorter.py
+++ image-sorter.py
@@ -1,1 +1,13 @@
 # initial
+from exif import Image
+
+
+def get_date(image: Image):
+    if image.has_exif:
+        return str(my_image.datetime).split(" ")[0].split(":")
+
+
+with open('example-images\\Camera\\20190803_083141.jpg', 'rb') as image_file:
+    my_image = Image(image_file)
+
+print(get_date(my_image))
